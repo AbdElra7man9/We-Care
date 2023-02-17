9b84cec54e78752f50776b73f059ed48325a8fb2
Mahmoud <re00zq@gmail.com>
2023-02-17 04:35:17 +0200

    fix some security issue

--- app.js
+++ app.js
@@ -1,14 +1,57 @@
 const express = require('express');
-const errorController = require('./controllers/errorController');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
+const errorController = require('./controllers/errorController');
 const doctorRouter = require('./routes/doctorRouter');
 const patientRouter = require('./routes/patientRouter');
 const userRouter = require('./routes/userRouter');
-
 const AppError = require('./utils/AppError');
 
 const app = express();
-app.use(express.json());
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 // doctors routs
 app.use('/api/v1/doctors', doctorRouter);
