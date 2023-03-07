3c5d4c296b02218eecaf6b3a90979d930f752193
AbdElra7man9 <abdelrahman201565@gmail.com>
2023-03-08 01:13:28 +0200

    no commit

--- client/src/Components/Dashboard/Patient/Layouts/Graphs/AreaPlot.js
+++ client/src/Components/Dashboard/Patient/Layouts/Graphs/AreaPlot.js
@@ -1,24 +1,41 @@
 
-import React, { useState, useEffect } from 'react';
 import { Area } from '@ant-design/plots';
 
 const AreaPlot = () => {
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
-        asyncFetch();
-    }, []);
-
-    const asyncFetch = () => {
-        fetch('https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json')
-            .then((response) => response.json())
-            .then((json) => setData(json))
-            .catch((error) => {
-                console.log('fetch data failed', error);
-            });
-    };
+    const data = [
+        {
+            "timePeriod": "2006 Q3",
+            "value": 1
+        },
+        {
+            "timePeriod": "2006 Q4",
+            "value": 3.08
+        },
+        {
+            "timePeriod": "2007 Q1",
+            "value": 2.17
+        },
+        {
+            "timePeriod": "2007 Q2",
+            "value": 1.26
+        },
+    ]
     const config = {
         data,
+        hight: '20rem',
+        autoFit: true,
+        smooth: true,
+        color: 'green',
+        yAxis: {
+            grid: {
+                line: {
+                    style: {
+                        opacity: .0,
+                    }
+                }
+            }
+        },
+        
         xField: 'timePeriod',
         yField: 'value',
         xAxis: {
@@ -26,7 +43,8 @@ const AreaPlot = () => {
         },
     };
 
-    return <Area {...config} />;
+    return <Area {...config
+    } />;
 };
 
 export default AreaPlot
