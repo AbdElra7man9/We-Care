e62c55a73d3f4307b6f4dcdf3f506ffbf41776aa
ahmedyousrygad <ahmedgad3772@gmail.com>
2023-03-15 16:57:13 +0200

    update and delete Review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -4,13 +4,15 @@ const AppError = require('../utils/AppError');
 const catchAsync = require('../utils/catchAsync');
 
 
-
 exports.makeReview = catchAsync(async function (req, res, next) {
     const patient = req.user.id;
     const doctor = req.params.id;
     const {rating , comment} = req.body;
+    
 
-
+    if(await Review.count( {patient : req.user.id , doctor : req.params.id} )>0)
+    return next(new AppError("you already made a review for this doctor before",401));
+    
     if(!await Doctor.findById(doctor).exec())
     return next(new AppError("this doctor is not exist",401));
 
@@ -24,11 +26,46 @@ exports.makeReview = catchAsync(async function (req, res, next) {
     if(comment.length>255)
     return next(new AppError("comment length shoud be less than 255 char",401));
 
-
-
     const newReview = await new Review({rating , comment, doctor , patient }).save()
     .then(()=>{return res.json({msg:"success"})})
-    .catch((error)=>{return console.log(error)})
+    .catch((error)=>{return next(new AppError("internal server error", 500))})
 });
 
 
+exports.updateReview = catchAsync(async function (req, res, next) {
+    const review = await Review.findOne({patient : req.user.id , doctor : req.params.id});
+
+    if(!review)
+    return next(new AppError("u didnt give this doctor rate befor",401));
+
+    if(!await Doctor.findById(req.params.id).exec())
+    return next(new AppError("this doctor is not exist",401));
+
+    if(!req.body.rating)
+    return next(new AppError("you shoud give rate for the doctor",401));
+
+    const ratingvalues =[1,2,3,4,5]
+    if(!ratingvalues.includes(req.body.rating))
+    return next (new AppError('not applicable entering rating ',401));
+
+    if(req.body.comment.length>255)
+    return next(new AppError("comment length shoud be less than 255 char",401));
+
+    const updateReview = await review.updateOne({rating : req.body.rating , comment : req.body.comment})
+    .then(()=>{return res.json({msg:"success"})})
+    .catch((error)=>{return next(new AppError("internal server error", 500))})
+});
+
+
+exports.deleteReview = catchAsync(async function(req, res, next){
+    const review = await Review.findOne({patient : req.user.id , doctor : req.params.id});
+
+    if(!review)
+    return next(new AppError("u didnt give this doctor rate befor",401));
+
+
+    await Review.findOneAndRemove({patient : req.user.id , doctor : req.params.id})
+    .then(()=>{return res.json({msg:"success"})})
+    .catch((error)=>{return next(new AppError("internal server error", 500))})
+
+});
--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -7,7 +7,8 @@ const router = express.Router();
 
 
 router.post('/makereview/:id', protect, reviewController.makeReview);
-
+router.patch('/updatereview/:id', protect, reviewController.updateReview);
+router.delete('/deletereview/:id', protect, reviewController.deleteReview);
 
 
 module.exports = router;
