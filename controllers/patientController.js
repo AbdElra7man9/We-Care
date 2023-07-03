ddecae3f3f25ac99c4f11d1d09f32fff2304f0bf
Mahmoud <re00zq@gmail.com>
2023-07-03 20:18:37 +0300

    add router to patient get all his appointments

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,14 +1,32 @@
 const catchAsync = require('../utils/catchAsync');
 const Patient = require('../Models/patientModel');
+const Appointment = require('../Models/appointmentModel');
 const Features = require('../utils/Features');
 
 exports.GetAllPatients = catchAsync(async (req, res, next) => {
-    const features = new Features(Patient.find(), req.query)
-        .Paginate();
-    const Patients = await features.query;
-    res.json({
-        status: 'success',
-        results: Patients.length,
-        Patients,
-    });
+  const features = new Features(Patient.find(), req.query).Paginate();
+  const Patients = await features.query;
+  res.json({
+    status: 'success',
+    results: Patients.length,
+    Patients,
+  });
+});
+
+exports.getAllApointmentsForPatient = catchAsync(async (req, res, next) => {
+  const pastAppointment = [];
+  const upcomingApointments = [];
+  const patient = req.user;
+  const allAppointments = await Appointment.find({ patient: patient._id });
+  allAppointments.forEach((appointment) => {
+    if (appointment.date > Date.now()) upcomingApointments.push(appointment);
+    if (appointment.date < Date.now() || appointment.date === Date.now())
+      pastAppointment.push(appointment);
+  });
+  res.status(200).json({
+    status: 'success',
+    results: allAppointments.length,
+    pastAppointment,
+    upcomingApointments,
+  });
 });
--- routes/patientRouter.js
+++ routes/patientRouter.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const { patientSignUP } = require('../controllers/authController');
-const { GetAllPatients } = require('../controllers/patientController');
+const {
+  GetAllPatients,
+  getAllApointmentsForPatient,
+} = require('../controllers/patientController');
+const protect = require('../Middlewares/protect');
+const restrictTo = require('../Middlewares/restrictTo');
 
 const router = express.Router();
 
 router.post('/signup', patientSignUP);
 router.get('/', GetAllPatients);
+router.get(
+  '/appointments',
+  protect,
+  restrictTo('Patient'),
+  getAllApointmentsForPatient
+);
 
 module.exports = router;
