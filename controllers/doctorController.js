6480d868736ec4d35aa49d3c045c953bb7a76b02
Mahmoud <re00zq@gmail.com>
2023-07-03 19:28:40 +0300

    fix some replication

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -3,48 +3,8 @@ const AppError = require('../utils/AppError');
 const catchAsync = require('../utils/catchAsync');
 const filterObject = require('../utils/filterObject');
 const Features = require('../utils/Features');
-
+const { specialists } = require('../data');
 exports.getAllSpecialists = (req, res, next) => {
-  const specialists = [
-    'Allergist',
-    'Andrologist',
-    'Anesthesiologist',
-    'Audiologist',
-    'Cardiologist',
-    'Cardiothoracic Surgeon',
-    'Dentist',
-    'Dermatologist',
-    'Endocrinologist',
-    'ENT Doctor (Otolaryngologist)',
-    'Family Doctor (General Practitioner)',
-    'Gastroenterologist',
-    'General Surgeon',
-    'Gynecologist',
-    'Hematologist',
-    'Hepatologist',
-    'Infertility Specialist',
-    'Internist',
-    'Laboratory',
-    'Nephrologist',
-    'Neurologist',
-    'Neurosurgeon',
-    'Nutritionist',
-    'Obesity Surgeon',
-    'Oncologist',
-    'Ophthalmologist',
-    'Orthopedist',
-    'Pediatric Surgeon',
-    'Pediatrician',
-    'Phoniatrician',
-    'Physiotherapist',
-    'Plastic Surgeon',
-    'Psychiatrist',
-    'Pulmonologist',
-    'Radiologist',
-    'Rheumatologist',
-    'Urologist',
-    'Vascular Surgeon',
-  ];
   res.json({
     status: 'success',
     reults: specialists.length,
