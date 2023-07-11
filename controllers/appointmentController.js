b7293d6654eaef7f7235089c844262df2593eb81
Mahmoud <re00zq@gmail.com>
2023-07-11 22:48:00 +0300

    x

--- controllers/appointmentController.js
+++ controllers/appointmentController.js
@@ -174,6 +174,7 @@ exports.getMyAppointments = catchAsync(async (req, res, next) => {
   if (user.__t == 'Doctor') {
     allAppointments = await Appointment.find({
       doctor: user._id,
+      status: 'booked',
     })
       .populate({ path: 'patient', select: ['name', 'profilePicture'] })
       .populate({ path: 'doctor', select: '_id' });
