036a3e9be39c3e52d2e04bd95338f9ca4f7364b2
AbdElra7man9 <abdelrahman201565@gmail.com>
2023-07-12 11:04:18 +0300

    fix merge confilect

--- routes/appointmentRouter.js
+++ routes/appointmentRouter.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const protect = require('../Middlewares/protect');
-const {
-  getAllDoctorAppointments,
-  getAvailableDoctorAppointments,
-  bookAppointment,
-  getAvailableDoctorAppointmentsByDay,
-  getAvailableDoctorDays,
-  getMyAppointments,
-  getMyBookedAppointments,
-  getAppointmentById,
-  completedOrNot,
-} = require('../controllers/appointmentController');
-const restrictTo = require('../Middlewares/restrictTo');
-
-const router = express.Router();
-
-router.get('/MyBooked', protect, restrictTo('Doctor'), getMyBookedAppointments);
-//protected routs
-router.get('/availableByday/:doctorID', getAvailableDoctorAppointmentsByDay);
-router.get('/availabledays/:doctorID', getAvailableDoctorDays);
-router.use(protect);
-router.get('/all/:doctorID', getAllDoctorAppointments);
-router.get('/available/:doctorID', getAvailableDoctorAppointments);
-router.get('/', getMyAppointments);
-router.get('/:appointmentId', getAppointmentById);
-//protected route and restrict to patient
-router.post('/book', restrictTo('Patient'), bookAppointment);
-router.post('/completedOrNot', restrictTo('Doctor'), protect , completedOrNot);
-
-//protected routs and restrict to doctor
-module.exports = router;
-=======
 const express = require('express');
 const protect = require('../Middlewares/protect');
 const {
@@ -62,4 +28,3 @@ router.post('/book', restrictTo('Patient'), bookAppointment);
 
 //protected routs and restrict to doctor
 module.exports = router;
->>>>>>> 8720b31 (updating profile)
